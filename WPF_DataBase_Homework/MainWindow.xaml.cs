42a67285a06550baa034629bbc276972fa99cdda
D4STEW143 <dastew143@mail.ru>
2025-05-11 18:18:53 +0900

    Visual undone

--- WPF_DataBase_Homework/MainWindow.xaml.cs
+++ WPF_DataBase_Homework/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,8 +19,36 @@ namespace WPF_DataBase_Homework {
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
 	public partial class MainWindow : Window {
+		Connector connector;
+
+		Query[] queries = new Query[]
+			{
+				new Query("*", "Students JOIN Groups ON([group]=group_id) JOIN Directions ON(direction=direction_id)"),
+				new Query(
+					"group_id,group_name,COUNT(stud_id) AS students_count,direction_name",
+					"Students,Groups,Directions",
+					"direction=direction_id AND [group]=group_id",
+					"group_id,group_name,direction_name"),
+				new Query(
+					@"direction_name,
+                    COUNT(DISTINCT group_id) AS N'Количество групп',
+                    COUNT(stud_id) AS N'Количество студентов'",
+					@"Students
+                    JOIN Groups ON ([group] = group_id)
+                    RIGHT JOIN Directions ON (direction = direction_id)",
+					"",
+					"direction_name"), //Tables
+
+				new Query("*", "Disciplines"),
+				new Query("*", "Teachers"),
+			};
+		DataGrid[] tables;
 		public MainWindow() {
 			InitializeComponent();
+
+			
+
+			
 		}
 	}
 }
