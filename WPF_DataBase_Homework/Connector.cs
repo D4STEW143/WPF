bfdeb41ed41a56f0a762addfa414a8861c3b4794
D4STEW143 <dastew143@mail.ru>
2025-05-11 12:11:57 +0900

    Before injections

---
+++ WPF_DataBase_Homework/Connector.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WPF_DataBase_Homework 
+{
+    internal class Connector
+    {
+        readonly string CONNECTION_STRING;
+        SqlConnection connection;
+        public Connector(string connection_string) {
+            CONNECTION_STRING = connection_string;
+            connection = new SqlConnection(CONNECTION_STRING);
+            AllocConsole();
+            Console.WriteLine(CONNECTION_STRING);
+        }
+        public DataTable Select(string colums, string tables, string condition = "", string group_by = "") {
+            DataTable table = null;
+            string cmd = $"SELECT {colums} FROM {tables}";
+            if (condition != "") cmd += $" WHERE {condition}";
+            if (group_by != "") cmd += $" GROUP BY {group_by}";
+
+            SqlCommand command = new SqlCommand(cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows){
+                table = new DataTable();
+                for(int i = 0; i <reader.FieldCount; i++){
+                    table.Columns.Add(reader.GetName(i));
+                }
+                while (reader.Read()){
+                    DataRow row = table.NewRow();
+                    for(int i = 0; i < reader.FieldCount; i++) row[i] = reader[i];
+                    table.Rows.Add(row);
+                }
+            }
+            reader.Close();
+            connection.Close();
+            return table;
+        }
+        public Dictionary<string, int> GetDictionary(string table) {
+            Dictionary<string, int> dictionary = null;
+            string id_collum = table.ToLower().Remove(table.Length - 1) + "_id";
+            string name_collum = table.ToLower().Remove(table.Length-1, 1) + "_name";
+            string cmd = $"SELECT {name_collum},{id_collum} FROM {table}";
+            SqlCommand command = new SqlCommand (cmd, connection);
+            connection.Open();
+            SqlDataReader reader = command.ExecuteReader();
+            if (reader.HasRows){
+                dictionary = new Dictionary<string, int>();
+                while (reader.Read()) {
+                    dictionary[reader[0].ToString()] = Convert.ToInt32(reader[1]);
+                }
+            }
+            reader.Close();
+            connection.Close();
+            return dictionary;
+        }
+        [DllImport("kernel32.dll")]
+        public static extern bool AllocConsole();
+        [DllImport("kernel32.dll")]
+        public static extern bool FreeConsole();
+    }
+}
---
+++ WPF_DataBase_Homework/Query.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_DataBase_Homework 
+{
+	internal class Query
+	{
+		public string Colums {  get; set; }
+		public string Tables { get; set; }
+		public string Condition { get; set; }
+		public string GroupBy { get; set; }
+		public Query(string colums, string tables, string condition = "", string groupBy = "")
+		{
+			Colums = colums;
+			Tables = tables;
+			Condition = condition;
+			GroupBy = groupBy;
+		}
+		public Query(Query other) { 
+			this.Colums = other.Colums;
+			this.Tables = other.Tables;
+			this.Condition = other.Condition;
+			this.GroupBy = other.GroupBy;
+		}
+
+	}
+}
